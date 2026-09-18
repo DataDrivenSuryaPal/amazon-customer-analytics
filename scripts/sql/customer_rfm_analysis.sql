9e329f47ed9462f9fb66fa2d71c2e63f4cb32a99
Surya pal <palsurya.947@gmail.com>
2026-09-18 22:00:50 +0530

    Add customer RFM analysis SQL view
    This SQL script creates a view for customer recency, frequency, and monetary analysis, categorizing customers into segments based on their scores.

---
+++ scripts/sql/customer_rfm_analysis.sql
@@ -0,0 +1,67 @@
+/*
+==============================================================================
+==============================================================================
+
+Customer recency_frequency_monetary analysis
+
+================================================================================
+================================================================================
+Purpose:
+ -This report shows the customer recency, frequency and monetary data
+Highlights:
+1. Gather essential fields such as CustomerID, CustomerName, orderdate, OrderID.
+2.Segment Customer categories into Best Customer, At risk customer, Lost customer
+  and others
+3. Aggregates Customer level matrics :
+ -Total Spend
+ -Total orders
+================================================================================
+================================================================================
+*/
+IF OBJECT_ID ('customer_rfm_analysis','v')
+    IS NOT NULL
+    DROP VIEW customer_rfm_analysis;
+GO
+CREATE VIEW customer_rfm_analysis AS
+WITH rfm_raw AS (
+    SELECT
+        s.CustomerID,
+        c.CustomerName,
+        DATEDIFF (Year,MAX(t.OrderDate),'2024-12-31') AS recency,
+        COUNT (DISTINCT s.OrderID) AS frequency,
+        SUM (s.TotalAmount) AS monetary
+    FROM fact_sale AS s
+    LEFT JOIN dim_time AS t
+        ON s.DateKey = t.DateKey
+    LEFT JOIN dim_customer AS c
+     ON s.CustomerID = c.CustomerID
+    GROUP BY s.CustomerID,
+        c.CustomerName),
+rfm_score AS (
+    SELECT
+        CustomerID,
+        CustomerName,
+        recency,
+        frequency,
+        monetary,
+        NTILE (5) OVER (ORDER BY recency) AS r_score,
+        NTILE (5) OVER (ORDER BY frequency) AS f_score,
+        NTILE (5) OVER (ORDER BY monetary) AS m_score
+    FROM rfm_raw),
+rfm_final AS(
+    SELECT 
+        CustomerID,
+        CustomerName,
+        r_score,
+        f_score,
+        m_score,
+        CASE 
+            WHEN r_score >= 4 AND f_score >= 4 AND m_score >= 4 THEN 'Best Customers'
+            WHEN r_score <= 2 AND f_score >= 3 AND m_score >= 3 THEN 'At-Risk Customers'
+            WHEN r_score <= 1 AND f_score <= 2 AND m_score <= 2 THEN 'Lost Customers'
+            ELSE 'Other'
+        END AS customer_segment
+    FROM rfm_score)
+SELECT
+*
+FROM rfm_final
